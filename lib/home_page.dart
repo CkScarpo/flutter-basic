1e7ea4810f4658fbfbae47cafb73ca7fc75e53cc
Luiz Eduardo <luizscarpo@gmail.com>
2022-04-11 20:36:54 -0300

    StatefulWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
       body: Center(
-        child: Text('Você clicou ${controller.value}'),
+        child: Text('Você clicou $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
